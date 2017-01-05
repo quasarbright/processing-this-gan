4adea17006fd811a79f10d542c04f5ce54de3cc3
quasarbright <mthundershock@gmail.com>
2017-01-05 12:40:32 -0500

    deposit

--- BankAccount.java
+++ BankAccount.java
@@ -14,10 +14,13 @@ public class BankAccount {
     public BankAccount(String myName, double myBalance){
         name = myName;
         balance = myBalance;
-        interest = .05;
     }
     
     public String toString(){
         return "name: "+name+". balance: $"+balance+". interest: "+interest;
+    }
+    
+    public void deposit(double money){
+        balance+=money;
     }
 }
