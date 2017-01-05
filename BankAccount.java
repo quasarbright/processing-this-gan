108b67d6eaa9a501c9fb3a9065019efd3050a098
quasarbright <mthundershock@gmail.com>
2017-01-05 12:53:34 -0500

    started javadocs

--- BankAccount.java
+++ BankAccount.java
@@ -11,11 +11,20 @@ public class BankAccount {
     private String name;
     private double balance;
     
+    /**
+     * Constructor. Creates a bank account with a name and balance
+     *
+     * @param myName String containing the account owner's name
+     * @param myBalance the account balance 
+     */
     public BankAccount(String myName, double myBalance){
         name = myName;
         balance = myBalance;
     }
     
+    /**
+     * 
+     */
     public String toString(){
         return "name: "+name+". balance: $"+balance+". interest: "+interest;
     }
