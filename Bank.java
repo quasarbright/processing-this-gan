1dd2a1c194cf859e017fcd2d3c6d3450b1731f56
quasarbright <mthundershock@gmail.com>
2017-01-05 19:52:47 -0500

    fixed syntax and convention errors and increased dankness
    Corrected syntax and convention errors. Also made a Tokyo Ghoul joke instead of a Hatsune Miku Joke

--- Bank.java
+++ Bank.java
@@ -7,10 +7,10 @@ package BankAccount;
  * @version 1-5-17
  */
  public class Bank{
-    public staticvoid main(String[] args){
+    public static void main(String[] args){
         
-     BankAccout Treefiidy = new BankAccount("Treefiddy", 31.41);
-     BankAccount HatsuneMiku = new BankAccount("Hatsune Miku", 1462460.00);
-        
+     BankAccount treefiddy = new BankAccount("Treefiddy", 31.41);
+     BankAccount unravel = new BankAccount("Kaneki", 993.00);
+     
     }
  }
