f7798ea86c1004a3ac9b50f62ed1f19459146ebb
quasarbright <mthundershock@gmail.com>
2017-01-06 12:33:23 -0500

    did stuff with accounts

--- Bank.java
+++ Bank.java
@@ -7,8 +7,31 @@
  */
  public class Bank{
     public static void main(String[] args){
-    	BankAccount treeFiddy = new BankAccount("Tree Fiddy", 31.41);
-    	BankAccount unravel = new BankAccount("Kaneki", 993.00);
+    	BankAccount treeFiddy = new BankAccount("Tree Fiddy", 0.00);
+    	BankAccount unravel = new BankAccount("Kaneki", 1000.00);
      
+     System.out.println(treeFiddy.toString());
+     System.out.println(unravel.toString());
+     
+     treeFiddy.deposit(31.41);
+     unravel.withdraw(7.00);
+     
+     System.out.println(treeFiddy.toString());
+     System.out.println(unravel.toString());
+     
+     treeFiddy.deposit(31.41);
+     treeFiddy.withdraw(10.00);
+     unravel.withdraw(7.00);
+     unravel.withdraw(7.00);
+     unravel.withdraw(7.00);
+     unravel.withdraw(7.00);
+     
+     System.out.println(treeFiddy.toString());
+     System.out.println(unravel.toString());
+     
+     unravel.deposit(7.00);
+     
+     System.out.println(treeFiddy.toString());
+     System.out.println(unravel.toString());
     }
  }
