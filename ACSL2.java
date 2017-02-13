6e2b95840a1f83eb395f22bf41a9712ad3b21116
quasarbright <mthundershock@gmail.com>
2017-02-13 12:51:38 -0500

    Create ACSL2.java

---
+++ ACSL2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACSL2;
+import java.util.ArrayList;
+/**
+ *
+ * @author mdelmonaco
+ */
+public class AscendingStrings {
+    public static String one(String num,String prev, boolean atStart){
+        if(atStart){
+            for(int i = 0;i<num.length();i++){
+                
+            }
+        }
+        else{
+            
+        }
+    }
+    public static ArrayList<Integer> process(String num){
+        ArrayList<Integer> nums = new ArrayList<>();
+        
+    }
+    
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        String num = "31415926538";//should have an arrayList later
+        
+    }
+}
