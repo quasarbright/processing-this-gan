32e5b63a74a888a6aab4a624016309e7a6b010c0
quasarbright <mthundershock@gmail.com>
2017-02-16 12:26:18 -0500

    Create CheckingAccount.java

---
+++ CheckingAccount.java
@@ -0,0 +1,24 @@
+package BankAccount;
+
+/**
+ * creates a bank account object
+ * 
+ * @author mdelmonaco
+ * @version 2-16-17
+ */
+public class CheckingAccount extends BankAccount {
+    private int transNum;
+    
+    /**
+     * creates checking account object
+     * 
+     * @param myName account holder's name
+     * @param myBalance initial balance
+     * @param myTransNum number of transactions
+     */
+    public CheckingAccount(String myName, double myBalance, int myTransNum){
+        super(myName, myBalance);
+        transNum = myTransNum;
+    }
+    
+}
