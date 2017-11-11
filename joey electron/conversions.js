a97f61ce082090afae4f4021b3192522888ca8d8
quasarbright <mthundershock@gmail.com>
2017-11-11 15:43:02 -0500

    added conversions

--- joey electron/conversions.js
+++ joey electron/conversions.js
@@ -2,6 +2,7 @@ let conversions = {
     replacements: [
         ["flip a coin", "roll a quarter"],
         ["search for a better life", "buscar una vida mejor"],
+        ["^((?!up a ))gondola","up a gondola"],
         [" a ", " an "],
         ["^((?!joey ))electron", "joey electron"],
         ["the best song", ["unravel", "russian national anthem", "H3H3 theme", "anything from the NieR: Automata soundtrack"]],
@@ -14,6 +15,7 @@ let conversions = {
         ["supreme", "sheep"],
         ["cancer", ["gan", "toby", "dps instalocks", "Rich and Marvin", "the right skate", "people who don't understand why communism is the best economy", "supreme", "starting an entire convorsation just to say a subreddit", "english versions of anime openings", "To be fair, you have to have a very high IQ to understand Rick and Morty. The humour is extremely subtle, and without a solid grasp of theoretical physics most of the jokes will go over a typical viewer’s head. There’s also Rick’s nihilistic outlook, which is deftly woven into his characterisation- his personal philosophy draws heavily from Narodnaya Volya literature, for instance. The fans understand this stuff; they have the intellectual capacity to truly appreciate the depths of these jokes, to realise that they’re not just funny- they say something deep about LIFE. As a consequence people who dislike Rick & Morty truly ARE idiots- of course they wouldn’t appreciate, for instance, the humour in Rick’s existential catchphrase “Wubba Lubba Dub Dub,” which itself is a cryptic reference to Turgenev’s Russian epic Fathers and Sons. I’m smirking right now just imagining one of those addlepated simpletons scratching their heads in confusion as Dan Harmon’s genius wit unfolds itself on their television screens. What fools.. how I pity them. And yes, by the way, i DO have a Rick & Morty tattoo. And no, you cannot see it. It’s for the ladies’ eyes only- and even then they have to demonstrate that they’re within 5 IQ points of my own (preferably lower) beforehand. Nothin personnel kid"]],
         ["boobs", "dekapai"],
+        ["sombra", "up a gondola"],
         ["tree fiddy", "dekapai"],
         ["350", "dekapai"],
         ["mercy", ["dekapai","R2"]],
